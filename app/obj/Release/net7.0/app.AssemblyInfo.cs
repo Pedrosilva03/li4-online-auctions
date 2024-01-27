6bf90e1ff5b08d1c942ca985ea28837ed97112d2
Duarte Leitão <duarte0903@gmail.com>
2024-01-27 15:46:14 +0000

    otimizações no css

--- app/obj/Release/net7.0/app.AssemblyInfo.cs
+++ app/obj/Release/net7.0/app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f498e6be28dc8cdac46acc7f3fd2d663ec47c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
